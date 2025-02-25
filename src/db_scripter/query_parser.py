71b36a9e5c2febe0195443bf004ecc952cc3ac10
Stephen Booth <stephen.booth.za@gmail.com>
2025-02-25 21:38:38 +0200

    SB: changes to parsr

--- src/db_scripter/query_parser.py
+++ src/db_scripter/query_parser.py
@@ -1,5 +1,7 @@
-from src.db_scripter.common import is_str_char
 import itertools
+from enum import Enum
+
+from src.db_scripter.common import is_str_char
 
 
 class SqlToken(object):
@@ -33,6 +35,12 @@ class SqlLiteralToken(SqlToken):
         self.value = value.strip().replace("[", "").replace("]", "")
 
 
+class SqlNameToken(SqlToken):
+
+    def __init__(self, value:str):
+        self.value = value.strip().replace("[", "").replace("]", "")
+
+
 class SqlNotToken(SqlToken):
     ...
 
@@ -44,14 +52,20 @@ class SqlOperatorToken(SqlToken):
 class SqlBooleanOperatorToken(SqlToken):
     ...
 
+class SqlParseState(Enum):
+    Initial = 1
+    Select = 2
+    From = 3
+    Where = 4
 
 class Parser(object):
     """
     select_statement ::= SELECT [* | <field_list>] FROM <tables> [WHERE <expression>]
     field_list ::= <field_entry> [',' <field_list>]
-    field_entry ::= [<name> '.'] [<name> '.'] <name> [AS <name>]
-    name ::= ['['] <literal> [']']
-    tables ::= [<name> '.'] <name> [',' <tables>]
+    field_entry ::= <name> [AS <literal>]
+    name ::=  <bracketed_name> ['.' <bracketed_name> ['.' <bracketed_name>]]
+    bracketed_name ::= ['['] <literal> [']']
+    tables ::= <name> [',' <tables>]
     expression ::= ['('] <term> [AND | OR <expression>] [')']
     term ::= ['('] <field_entry> | <literal> [NOT] <operator> <field_entry> | <literal> [')']
     operator ::= '=' | '<' | '>' | '<>' | 'IS' | 'IS NOT'
@@ -60,11 +74,14 @@ class Parser(object):
     tokens: list[SqlToken]
     sql: str
     index: int
+    state:SqlParseState
 
     def __init__(self, sql: str):
         self.tokens = []
         self.sql = sql
         self.index = 0
+        self.state = SqlParseState.Initial
+
         while self.index < len(sql):
             c = self.sql[self.index]
             if c.isspace():
@@ -109,10 +126,13 @@ class Parser(object):
 
         str_upper = str.upper().strip()
         if str_upper == "SELECT":
+            self.state = SqlParseState.Select
             return SqlSelectToken(str)
         elif str_upper == "WHERE":
+            self.state = SqlParseState.Where
             return SqlWhereToken(str)
         elif str_upper == "FROM":
+            self.state = SqlParseState.From
             return SqlFromToken(str)
         elif str_upper == "NOT":
             return SqlNotToken(str)
@@ -120,12 +140,14 @@ class Parser(object):
             return SqlBooleanOperatorToken(str)
         elif str_upper == "OR":
             return SqlBooleanOperatorToken(str)
+        elif self.state == SqlParseState.Select or self.state == SqlParseState.From or "'" not in str:
+            return SqlNameToken(str)
         else:
             return SqlLiteralToken(str)
 
     def clean_tokens(self):
         # combine operators
-        new_tokens:list[SqlToken] = []
+        new_tokens: list[SqlToken] = []
         grouped = []
         key_func = lambda x: type(x)
         for key, group in itertools.groupby(self.tokens, key_func):
@@ -137,6 +159,10 @@ class Parser(object):
                 new_tokens.append(new_token)
             else:
                 new_tokens.extend(group)
+
+        # for i in range(len(new_tokens)):
+        #     token = new_tokens[i]
+        #     if token is SqlLiteralToken:
 
         # check for normal tokens
         # for i in range(len(new_tokens)):
@@ -157,4 +183,3 @@ class Parser(object):
         #             new_tokens[i] = SqlBooleanOperatorToken(token.value)
 
         self.tokens = new_tokens
-
--- tests/test_general.py
+++ tests/test_general.py
@@ -4,7 +4,7 @@ from sb_serializer import Naming, HardSerializer
 
 from src.db_scripter.database_objects import View
 from src.db_scripter.options import Options
-from src.db_scripter.query_parser import Parser, SqlSelectToken, SqlStarToken, SqlFromToken, SqlLiteralToken
+from src.db_scripter.query_parser import Parser, SqlSelectToken, SqlStarToken, SqlFromToken, SqlLiteralToken, SqlNameToken
 from tests.common import naming, serializer
 
 
@@ -23,13 +23,10 @@ class TestGeneral(unittest.TestCase):
 
     def test_dict(self):
         word = "ufnGetAccountingEndDate"
-        # naming = Naming("S:\\src\\pvt\\db_scripter\\dictionary.txt", "S:\\src\\pvt\\db_scripter\\bigworddictionary.txt")
         name = naming.string_to_name(word)
         print(name.name)
 
     def test_view(self):
-        # naming = Naming("S:\\src\\pvt\\db_scripter\\dictionary.txt", "S:\\src\\pvt\\db_scripter\\bigworddictionary.txt")
-        # serializer = HardSerializer(naming=naming)
         json = ""
         with open("test_view.json", 'r', encoding="utf-8") as file:
             json = file.read()
@@ -48,11 +45,11 @@ class TestGeneral(unittest.TestCase):
         self.assertTrue(type(parser.tokens[0]) == SqlSelectToken)
         self.assertTrue(type(parser.tokens[1]) == SqlStarToken)
         self.assertTrue(type(parser.tokens[2]) == SqlFromToken)
-        self.assertTrue(type(parser.tokens[3]) == SqlLiteralToken)
+        self.assertTrue(type(parser.tokens[3]) == SqlNameToken)
 
     def test_parser_mssql(self):
-        parser = Parser("select [name] from [dbo].[bob] where [name]<>'bob'")
-        self.assertTrue(len(parser.tokens) == 8)
+        parser = Parser("select [name] from [dbo].[bob] where [name]<>'bob' or [name] = 'bill'")
+        self.assertTrue(len(parser.tokens) == 12)
 
     def test_list(self):
         l1 = [1, 2, 3]
